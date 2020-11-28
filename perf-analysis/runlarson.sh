a0d933876d52595c529ee681df2ed7026da95a9d
root <root@red01.syn.uwaterloo.ca>
2020-11-27 23:44:48 -0500

    oops, bug fix

--- perf-analysis/runlarson.sh
+++ perf-analysis/runlarson.sh
@@ -6,5 +6,5 @@ if [ $malloclib == "malloc" ]
 then
 	perf stat -e dTLB-load-misses,iTLB-load-misses,dTLB-store-misses,cache-references,cache-misses  -x' ' -o $tmpfile ./larson  10 7 8 1000 10000 1 8
 else
-	perf stat -e dTLB-load-misses,iTLB-load-misses,dTLB-store-misses,cache-references,cache-misses -x' ' -o $tmpfile ./larson-mesh.sh $malloclib
+	perf stat -e dTLB-load-misses,iTLB-load-misses,dTLB-store-misses,cache-references,cache-misses -x' ' -o $tmpfile ./larson-ldpreload.sh $malloclib
 fi
